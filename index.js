525fd3c92a7a23af91bc8b3c7cff617d816e90ec
Arnold Lee <arnoldlee60@gmail.com>
2021-07-07 17:26:35 -0500

    trying to loop for all employees

--- index.js
+++ index.js
@@ -7,30 +7,65 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 console.log("=== Prompt ===");
 
+//create an array for your team 
+//and then prompt for info then ask if there are more pople on the team 
+const myTeam = [];
+
 const promptUser = () => {
     return inquirer.prompt([
       {
         type: 'input',
         name: 'name',
-        message: 'What is the name of your employee?',
+        message: 'What is the first and last name of your employee?',
+        disName: function getName() {
+            return this.name;
+        },
       },
       {
         type: 'input',
         name: 'id',
         message: 'What is the employee id?',
+        disId: function getId(){
+            return this.id;
+        }
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: "What the employee's email?",
+        disEmail: function getEmail(){
+            return this.email;
+        }
       },
       {
         type: 'input',
         name: 'email',
         message: "What the employee's email?",
       },
-      {
-        type: 'input',
-        name: 'email',
-        message: "What the employee's email?",
-      },
-    ]);
+    ]
+    );
   };
+
+
+
+  const promptUserAgain = () => {
+    inquirer.prompt(
+        {
+            type: 'list',
+            name: 'another',
+            message: "Is there another employee?",
+            choices: ["Yes", "No"],
+            again : function() {
+                if(this.choices == "Yes")
+                    {
+                        //console.log("hi")
+                      promptUser()
+                    }
+              }
+        },
+    )
+};
+
 
 const generateHTML = (answers) =>
   `<!DOCTYPE html>
@@ -42,15 +77,22 @@ const generateHTML = (answers) =>
     <title>Document</title>
   </head>
   <body>
-    
+  <p id="demo"></p>
+  ${answers.name}
   </body>
-  </html>`;
+  </html>`
+  ;
 
   const init = () => {
     promptUser()
-      .then((answers) => writeFileAsync('index.html', generateHTML(answers)))
-      .then(() => console.log('Successfully wrote to index.html'))
-      .catch((err) => console.error(err));
+      .then((answers) => promptUserAgain())
+      //.then((answers) => writeFileAsync('index.html', generateHTML(answers)))
+      //.then(() => console.log('Successfully wrote to index.html')) //'Successfully wrote to index.html'
+      //.catch((err) => console.error(err));
   };
   
   init();
+  
+
+ 
+
